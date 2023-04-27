de1c89f414ec1fa00614fd1da051dd44b1f56f94
payam mohajerin <payam3006@gmail.com>
2023-04-27 07:30:55 +0330

    final

--- script.js
+++ script.js
@@ -33,8 +33,11 @@ const mainContainer = document.getElementById("mainContainer");
 const fillList = () => {
   for (let i = 0; i < 10; i++) {
     mainContainer.innerHTML += `<div class="num">${i + 1}</div>
-    <div id="item${i + 1}" class="item">
-        <p>${randomisedTop10[i]}</p>
+    <div id="item${
+      i + 1
+    }" class="item" draggable="true" ondragstart="drag(event)" ondragover="allowDrop(event)"
+    ondragleave="leave(event)" ondrop="drop(event)">
+        <p class="text">${randomisedTop10[i]}</p>
         <div id="dragBtn${
           i + 1
         }" class="dragBtn"><i class="fa fa-reorder"></i></div>
@@ -43,3 +46,68 @@ const fillList = () => {
 };
 
 fillList();
+
+///////////////////////check!///////////////////////////
+function checkItems() {
+  for (let i = 0; i < 10; i++) {
+    let text = document
+      .getElementById(`item${i + 1}`)
+      .getElementsByTagName("p")[0];
+    // q(text);
+    if (text.innerText == top10[i].name) {
+      text.classList.add("correct");
+      text.classList.remove("incorrect");
+    } else {
+      text.classList.add("incorrect");
+      text.classList.remove("correct");
+    }
+  }
+}
+
+///////////////////////drag and drop...///////////////////
+const drag = (eve) => {
+  eve.dataTransfer.setData("originId", eve.target.id);
+  eve.dataTransfer.setData(
+    "originText",
+    document.getElementById(`${eve.target.id}`).getElementsByTagName("p")[0]
+      .innerText
+  );
+
+  // q(eve.dataTransfer.getData("originId"));
+};
+
+const drop = (eve) => {
+  eve.preventDefault();
+  // q(`${eve.target.id}`);
+  // q(`${eve.currentTarget.id}`);
+  document
+    .getElementById(`${eve.currentTarget.id}`)
+    .classList.remove("dropOver");
+
+  let originId = eve.dataTransfer.getData("originId");
+  let originText = eve.dataTransfer.getData("originText");
+  let targetText = document
+    .getElementById(`${eve.currentTarget.id}`)
+    .getElementsByTagName("p")[0].innerText;
+
+  document
+    .getElementById(`${originId}`)
+    .getElementsByTagName("p")[0].innerText = targetText;
+
+  document
+    .getElementById(`${eve.currentTarget.id}`)
+    .getElementsByTagName("p")[0].innerText = originText;
+};
+
+const allowDrop = (eve) => {
+  eve.preventDefault();
+  document.getElementById(`${eve.currentTarget.id}`).classList.add("dropOver");
+  // eve.dataTransfer.dropEffect = "move";
+};
+
+const leave = (eve) => {
+  eve.preventDefault();
+  document
+    .getElementById(`${eve.currentTarget.id}`)
+    .classList.remove("dropOver");
+};
